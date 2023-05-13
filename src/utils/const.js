fb0bfe326774341226a85d2b699788be772dfbe1
dip <dip.v@immence.in>
2023-05-13 23:48:39 +0530

    ops:fix

--- src/utils/const.js
+++ src/utils/const.js
@@ -1,3 +1,3 @@
-export const API_URL = 'http://localhost-api.immence.in:3000/';
-// export const API_URL = 'https://stg-api.azle.io/';
+// export const API_URL = 'http://localhost-api.immence.in:3000/';
+export const API_URL = 'https://stg-api.azle.io/';
 export const API_VERSION = 'v1';
